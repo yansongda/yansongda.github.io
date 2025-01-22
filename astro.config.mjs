945c8ed9af206cbe419baeb97c5991c0977803f6
yansongda <me@yansongda.cn>
2025-01-22 13:30:14 +0800

    chore: 优化构建输出与 url 格式

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yansongda.cn',
+  trailingSlash: "always",
 	integrations: [tailwind()],
+  build: {
+    format: "directory",
+  }
 });

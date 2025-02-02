b582562c68f16aa63f6289c83b842063483f8ea4
yansongda <me@yansongda.cn>
2025-02-02 22:34:40 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 import { defineConfig } from "astro/config";
 
-import tailwindcss from "@tailwindcss/vite";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yansongda.cn',
   trailingSlash: "ignore",
-	integrations: [],
-  vite: {
-    plugins: [tailwindcss],
-  },
+	integrations: [tailwind()],
   build: {
     format: "directory",
   }

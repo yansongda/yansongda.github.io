c48a869c53faf5363ac2479e85c3e3880c2c99d4
yansongda <me@yansongda.cn>
2025-02-27 09:02:41 +0800

    chore: upgrade tailwindCSS to v4 (#6)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yansongda.cn',
   trailingSlash: "ignore",
-	integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   build: {
     format: "directory",
   }
